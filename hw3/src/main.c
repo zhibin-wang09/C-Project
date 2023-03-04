457c797e4c40a3d0fcebf57579fb020048d38322
Gene Stark <gene@starkeffect.com>
2023-03-04 15:42:47 -0500

    Remove WEAK_MAGIC, which is not used.

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include "sfmm.h"
 
-/*
- * Define WEAK_MAGIC during compilation to use MAGIC of 0x0 for debugging purposes.
- * Note that this feature will be disabled during grading.
- */
-#ifdef WEAK_MAGIC
-int sf_weak_magic = 1;
-#endif
-
 int main(int argc, char const *argv[]) {
     double* ptr = sf_malloc(sizeof(double));
 
