90b644be359b1446a7a6ad8fb00462a9cebb73bb
Zhi Bin Wang <zhibin.wang@stonybrook.edu>
2023-04-03 13:18:50 -0400

    change stdin to non-block and async

--- hw4/src/ticker.c
+++ hw4/src/ticker.c
@@ -1,40 +1,101 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include "ticker.h"
 
 typedef struct watcher{
+    pid_t pid;
     int enable;
+    int inputfd;
+    int outputfd;
+    char *name;
 } WATCHER;
 
-int msg_ready(int sig){
-    return 0;
-}
+typedef struct{
+    pid_t pid;
+    WATCHER watcher;
+    int index;
+} link_table;
 
-void handler(int sig);
+void terminated(int sig, siginfo_t *info, void * ucontext);
+void msg_ready(int sig, siginfo_t *info, void * ucontext);
+void fctnl_setup();
 
 int ticker(void) {
-    // TO BE IMPLEMENTED
-
-    struct sigaction CLI = {0};
-    CLI.sa_handler = handler;
-    sigaction(SIGINT, &CLI, NULL);
-    char input[100];
-    while(1){ // as long as the user did not quit the program keep listening for input
-        *input = 0;
-        printf("ticker> ");
-        fgets(input,100,stdin);
-        printf("%s",input);
-        sigsuspend(&(CLI.sa_mask));
-        printf("resume\n");
+    sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set,SIGQUIT);
+    sigdelset(&set,SIGINT);
+    sigdelset(&set,SIGCHLD);
+    sigdelset(&set,SIGIO);
+    struct sigaction termination = {0}; // the signal for notifying that a watcher process has terminated
+    termination.sa_sigaction = terminated;
+    termination.sa_flags = SA_SIGINFO;
+    sigemptyset(&termination.sa_mask);
+    if(sigaction(SIGCHLD, &termination, NULL) < 0){
+        perror("Creating SIGCHILD failed");
+        return -1;
     }
 
-    //reap all the remaining watchers
 
-    //kill the CLI watcher as well
+    struct sigaction notification = {0}; // the signal for notifying that a message from watcher is ready
+    notification.sa_flags = SA_SIGINFO;
+    notification.sa_sigaction = &msg_ready;
+    sigemptyset(&notification.sa_mask);
+    if(sigaction(SIGIO, &notification, NULL) < 0){
+        perror("Creating SIGIO failed");
+        return -1;
+    }
+
+    fctnl_setup();
+
+    char input[100];
+    int c;
+    while(1){ // as long as the user did not quit the program keep listening for input
+        *input = 0;
+
+        printf("ticker> ");
+        fflush(stdout);
+        // Waits for user input with current process suspended
+        sigsuspend(&set);
+        read(STDIN_FILENO,input,100); // read whatever
+        if(errno == EWOULDBLOCK) // if nothing continue
+            continue;
+        FILE *inputf =fmemopen(input,strlen(input),"r");
+        while((c = fgetc(inputf)) != EOF){
+            
+        } // read the input char by char
+
+    }
+}
+
+/*
+    signal handler for notification of a message from a watcher process
+*/
+void msg_ready(int sig, siginfo_t *info, void * ucontext){
+    printf("SIGIO FIRED\n");
+}
+
+/*
+    signal handler for termination of a watcher proces
+*/
+void terminated(int sig, siginfo_t *info, void * ucontext){
+    //reap the terminated child
 }
 
 
-void handler(int sig){
-    printf("%d\n",sig);
+void fctnl_setup(){
+    if(fcntl(STDIN_FILENO,F_SETFL, fcntl(0, F_GETFL) |O_ASYNC|O_NONBLOCK) < 0){
+        perror("Setting STDIN to async failed");
+    }
+    if(fcntl(STDIN_FILENO,F_SETOWN,getpid()) < 0){
+        perror("Setting receving process of SIGIO signal failed");
+    }
+    if(fcntl(STDIN_FILENO,F_SETSIG,SIGIO) < 0){
+        perror("Setting signal handler for async STDIN failed.");
+    }
 }
