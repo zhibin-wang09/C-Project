7578c356f762a20c2824022cb9012e05cd6b13d2
Zhi Bin Wang <zhibin.wang@stonybrook.edu>
2023-04-04 23:49:29 -0400

    implement the reading loop

--- hw4/src/ticker.c
+++ hw4/src/ticker.c
@@ -26,14 +26,19 @@ void terminated(int sig, siginfo_t *info, void * ucontext);
 void msg_ready(int sig, siginfo_t *info, void * ucontext);
 void fctnl_setup(int fd);
 void evaluate(char input[],size_t size);
-volatile int ready= 0;
 //char *dynamic_resize(char input[],int cur_size);
 
 int ticker(void) {
     sigset_t set;
+    sigfillset(&set);
+    sigdelset(&set,SIGIO);
+    sigdelset(&set,SIGCHLD);
+    sigdelset(&set,SIGINT);
+    sigdelset(&set,SIGQUIT);
+    sigdelset(&set,SIGTERM);
+    sigdelset(&set,SIGSTOP);
     struct sigaction termination = {0}; // the signal for notifying that a watcher process has terminated
     termination.sa_sigaction = terminated;
-    termination.sa_flags = SA_SIGINFO;
     sigemptyset(&termination.sa_mask);
     if(sigaction(SIGCHLD, &termination, NULL) < 0){
         perror("Creating SIGCHILD failed");
@@ -53,38 +58,22 @@ int ticker(void) {
     fctnl_setup(STDIN_FILENO);
     int cur_length = 128;
     char input[cur_length]; //calloc(cur_length,1); // need to be dynamically resized
-   // int bytes_read = 0;
     int ret = 0;
-    memset(input,0,cur_length);
-    if((ret = read(STDIN_FILENO,input,cur_length)) < 0 && errno != EWOULDBLOCK){
-            perror("Reading input failed");
-    }
-    if(errno != EWOULDBLOCK) {ready = 1;}
-    printf("%d, %d\n",ret,errno==EWOULDBLOCK);
+    int ticker_print = 1;
     while(1){ // as long as the user did not quit the program keep listening for input
-        printf("ticker> ");
-        fflush(stdout);
-        // Waits for user input with current process suspended
-        if(ready == 0){
-            sigsuspend(&set);
-            memset(input,0,cur_length);
-            if((ret = read(STDIN_FILENO,input,cur_length)) < 0 && errno != EAGAIN){
-                perror("Reading input failed");
-            }
+        if(ticker_print == 1){
+            printf("ticker> ");
+            fflush(stdout);
         }
-        // while(bytes_read = (read(STDIN_FILENO,input,cur_length))!=0){
-        //     if(bytes_read == -1){
-        //         perror("Reading input failed");
-        //     }
-        //     int input_length = strlen(input);
-        //     if(input_length >= sizeof(input)){
-        //                     printf("%s\n",input);
-        //         input = dynamic_resize(input,cur_length);
-        //         cur_length*=2;
-        //     }
-        // } // read whatever
+        // Waits for user input with current process suspended
+        memset(input,0,cur_length);
+        if((ret = read(STDIN_FILENO,input,cur_length)) < 0 && errno != EWOULDBLOCK){
+                perror("Reading input failed");
+        }
+        //printf("%d, %d\n",ret,errno==EWOULDBLOCK);
+        if(errno == EWOULDBLOCK) {sigsuspend(&set); ticker_print = 0;continue;}
         evaluate(input, sizeof(input));
-        ready = 0;
+        ticker_print = 1;
     }
     return 0;
 }
@@ -92,7 +81,7 @@ int ticker(void) {
     signal handler for notification of a message from a watcher process
 */
 void msg_ready(int sig, siginfo_t *info, void * ucontext){
-    fprintf(stderr, "msg ready\n");
+    //write(STDIN_FILENO, "msg ready\n",10);
 }
 
 /*
@@ -104,7 +93,7 @@ void terminated(int sig, siginfo_t *info, void * ucontext){
 
 
 void fctnl_setup(int fd){
-    if(fcntl(fd,F_SETFL, fcntl(0, F_GETFL) |O_ASYNC|O_NONBLOCK) < 0){
+    if(fcntl(fd,F_SETFL, O_ASYNC|O_NONBLOCK) < 0){
         perror("Setting STDIN to async failed");
     }
     if(fcntl(fd,F_SETOWN,getpid()) < 0){
@@ -116,6 +105,7 @@ void fctnl_setup(int fd){
 }
 
 void evaluate(char input[], size_t size){
+    if(strlen(input) == 0) exit(-1);
     char *str  = strtok(input, " ");
     if(strcmp(str, "start") == 0){
         printf("start\n");
