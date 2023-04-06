e019480b264dee4482ae1efb9b036c6cd65bf372
Zhi Bin Wang <zhibin.wang@stonybrook.edu>
2023-04-05 20:57:03 -0400

    change read to byte by byte

--- hw4/src/ticker.c
+++ hw4/src/ticker.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include "ticker.h"
+#include "cli.h"
 
 typedef struct watcher{
     pid_t pid;
@@ -14,7 +15,6 @@ typedef struct watcher{
     int outputfd;
     char *name;
 } WATCHER;
-
 typedef struct link_list{
     int index;
     pid_t pid;
@@ -22,10 +22,12 @@ typedef struct link_list{
     struct link_list *next;
 } TABLE;
 
+
+
 void terminated(int sig, siginfo_t *info, void * ucontext);
 void msg_ready(int sig, siginfo_t *info, void * ucontext);
 void fctnl_setup(int fd);
-void evaluate(char input[],size_t size);
+void evaluate(char input[],int *stop);
 
 int ticker(void) {
     sigset_t set;
@@ -54,45 +56,45 @@ int ticker(void) {
         return -1;
     }
 
-    fctnl_setup(STDIN_FILENO);
+     fctnl_setup(STDIN_FILENO);
+    // WATCHER_TYPE cli_watcher = {};
+    // cli_watcher_start(&cli_watcher,NULL);
+
     int cur_length = 128;
     int bytes_read = 0;
     char *input = malloc(cur_length); //calloc(cur_length,1); // need to be dynamically resized
     int ret = 0;
     int ticker_print = 1;
+    int stop = 0;
     while(1){ // as long as the user did not quit the program keep listening for input
         if(ticker_print == 1){
             printf("ticker> ");
             fflush(stdout);
         }
         // Waits for user input with current process suspended
-        memset(input,0,cur_length);
-        while((ret = read(STDIN_FILENO,input+bytes_read,cur_length)) > 0){
+        memset(input+bytes_read,0,cur_length);
+        while((ret = read(STDIN_FILENO,input+bytes_read,1)) > 0){
             bytes_read += ret;
+            if(input[bytes_read-1] == '\n') break;
             if(bytes_read >= cur_length){
                 input = realloc(input,cur_length*2);
                 cur_length *=2;
             }
         }
-        //printf("ret: %d, errno: %d,bytesread: %d,cur_length: %d, input: %s\n",ret,errno,bytes_read,cur_length,input);
         if(ret == -1 && errno != EWOULDBLOCK) perror("Reading input failed");
         if(errno == EWOULDBLOCK) { // in the case where read has read everything or there is not input
-            if(bytes_read != 0){ // input is ready and stored in buffer
-                evaluate(input, sizeof(input));
-                ticker_print = 1;
-                bytes_read =0;
-                continue;
-            }else{ // no input read
-                ticker_print =0;
-            }
             sigsuspend(&set); // stop until input is read
-            bytes_read = 0;
+            ticker_print = 0;
             continue;
         }
-        if(ret == 0){ // ret is 0 means input from echo
-            evaluate(input, sizeof(input));
+        if(ret == 1){
+            memset(input + bytes_read, 0, cur_length - bytes_read);
+            evaluate(input+stop,&stop);
             ticker_print = 1;
-            bytes_read = 0;
+        }
+        if(ret == 0 ){ // ret is 0 means EOF
+            free(input);
+            exit(0);
         }
     }
     return 0;
@@ -124,34 +126,42 @@ void fctnl_setup(int fd){
     }
 }
 
-void evaluate(char input[], size_t size){
-    if(strlen(input) == 0) exit(-1);
-    char *str  = strtok(input, " ");
-    if(strcmp(str, "start") == 0){
-        printf("start\n");
-        str = strtok(NULL,"\n");
-        printf("%s\n",str);
-    }else if(strcmp(str, "watchers\n") == 0){
-        printf("watchers\n");
-    }else if(strcmp(str, "stop") == 0){
-        printf("stop\n");
-        str = strtok(NULL,"\n");
-        printf("%s\n",str);
-    }else if(strcmp(str, "show") == 0){
-        printf("show\n");
-        str = strtok(NULL,"\n");
-        printf("%s\n",str);
-    }else if(strcmp(str, "trace") == 0){
-        printf("trace\n");
-        str = strtok(NULL,"\n");
-        printf("%s\n",str);
-    }else if(strcmp(str,"untrace") == 0){
-        printf("untrace\n");
-        str = strtok(NULL,"\n");
-        printf("%s\n",str);
-    }else if(strcmp(str, "quit\n") == 0){
-        printf("quit\n");
-    }else{
-        printf("???\n");
+void evaluate(char input[],int *stop){
+
+    char *arg = input;
+    char *ptr = input;
+    int counter =0;
+    while(*arg != '\n'){ // parse the command
+        arg++;
+        counter++;
     }
+    input[counter] = '\0';
+    char *command = strtok(ptr," ");
+    char *args = strtok(NULL,"");
+    if(args != NULL){
+        if(strcmp(command, "start") == 0){
+            printf("start\n");
+        }else if(strcmp(command,"show") == 0){
+            printf("show\n");
+        }else if(strcmp(command, "trace") == 0){
+            printf("trace\n");
+        }else if(strcmp(command, "untrace") == 0){
+            printf("untrace\n");
+        }else if(strcmp(command,"stop") == 0){
+            printf("stop\n");
+        }else {
+            printf("???\n");
+        }
+    }else{
+         if(strcmp(command, "watchers") == 0){
+            printf("watchers\n");
+        }else if(strcmp(command, "quit") == 0){
+            printf("quit\n");
+        }else{
+            printf("???\n");
+        }
+    }
+    input[counter] = '\n';
+    ptr = arg+1;
+    *stop = counter+1 + *stop;
 }
