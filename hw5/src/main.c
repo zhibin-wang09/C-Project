06c3a6c200bddff197c870385029be27c5a2f6ea
Zhi Bin Wang <zhibin.wang@stonybrook.edu>
2023-04-19 22:12:55 -0400

    Check the argument passed to the program and create socket base on the port argument, on success a thread is created to handle the connection

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -8,6 +8,8 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <errno.h>
 
 #include "debug.h"
 #include "protocol.h"
@@ -21,6 +23,8 @@ int _debug_packets_ = 1;
 #endif
 
 static void terminate(int status);
+void terminal_is_closed(int sig, siginfo_t *act, void* context);
+int get_listenfd(char *port);
 
 /*
  * "Jeux" game server.
@@ -31,7 +35,19 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
+    int port;
+    if(argc != 3) return -1; // no -p <port>
+    if(strcmp(argv[1],"-p")) return -1; // option is not -p
+    port = (int)strtol(argv[2],NULL,10);
+    if(port < 1024) return -1; // port is declared int the range of reserved ports
 
+    struct sigaction terminal_closed = {0};
+    terminal_closed.sa_sigaction = terminal_is_closed; // SIGHUP handler
+    sigemptyset(&terminal_closed.sa_mask);
+    if(sigaction(SIGHUP, &terminal_closed,NULL) < 0){ // install handler
+        perror("SIGHUP install error");
+        return -1;
+    }
     // Perform required initializations of the client_registry and
     // player_registry.
     client_registry = creg_init();
@@ -43,10 +59,74 @@ int main(int argc, char* argv[]){
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
 
+    int listenfd;
+    if((listenfd = get_listenfd(argv[2]) < 0)){
+        return -1;
+    }
+
+    int *connfd;
+    socklen_t clientaddr_len;
+    struct sockaddr clientaddr;
+    pthread_t tid;
+    while(1){
+        clientaddr_len = sizeof(struct sockaddr);
+        connfd = malloc(sizeof(*connfd));
+        if((*connfd = accept(listenfd, &clientaddr, &clientaddr_len)) < 0){
+            perror("creating communication sock file descriptor failed\n");
+            fprintf(stderr, "errno : %d\n",errno);
+        }
+        if(pthread_create(&tid, NULL, &jeux_client_service,connfd)){
+            perror("start thread for jeux client service failed\n");
+            fprintf(stderr, "errno : %d\n",errno);
+        }
+
+    }
+
+
     fprintf(stderr, "You have to finish implementing main() "
 	    "before the Jeux server will function.\n");
 
     terminate(EXIT_FAILURE);
+}
+
+void terminal_is_closed(int sig, siginfo_t *act, void* context){
+
+}
+
+int get_listenfd(char *port){
+    struct addrinfo hint, *res, *ptr;
+    int listenfd, optval=1;
+    memset(&hint, 0,sizeof(struct addrinfo));
+    hint.ai_socktype = SOCK_STREAM; // SOCK_STREAM indicates that this socket will be used as an end point connection
+    hint.ai_flags = AI_NUMERICSERV | AI_PASSIVE | AI_ADDRCONFIG; // indicate that socket uses port number as service, , is an listening socket, and is a connection
+    if(getaddrinfo(NULL,port,&hint,&res)){ // get the list of addresses
+        perror("Get addr failed\n");
+        fprintf(stderr, "errno : %d\n", errno);
+        return -1;
+    }
+
+    for(ptr = res; ptr ; ptr = ptr -> ai_next){
+        //creates a socket descriptor
+        if((listenfd  = socket(ptr->ai_family, ptr -> ai_socktype, ptr -> ai_protocol) < 0)) continue; // create socket connection
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,    //line:netp:csapp:setsockopt
+                   (const void *)&optval , sizeof(int));
+        if(!bind(listenfd, ptr -> ai_addr, ptr -> ai_addrlen)) break; // create server side endpoint
+        if(close(listenfd) < 0){perror("close socket failed\n"); return -1;} // close listenfd because it is no longer needed since it didn't work with bind
+    }
+
+    freeaddrinfo(res); // necessary to free the return list
+    if(!ptr){ // no address succeeded
+        perror("Could not bind\n");
+        return -1;
+    }
+
+    if(listen(listenfd, 1024) < 0){
+        perror("Converting descriptor to listen descriptor failed\n");
+        fprintf(stderr, "errno: %d\n",errno);
+        close(listenfd);
+        return -1;
+    }
+    return listenfd;
 }
 
 /*
