d5f6a5e29e2fe40aedb05e727e0d21c2e248af07
Zhi Bin Wang <zhibin.wang@stonybrook.edu>
2023-04-25 21:38:42 -0400

    implement invitation and fix segmentation fault that was caused by setting the game pointer to null

--- hw5/src/invitation.c
+++ hw5/src/invitation.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <pthread.h>
-#include <semaphore.h>
 #include <debug.h>
 
 #include "client_registry.h"
@@ -20,13 +19,12 @@ struct invitation{
 	GAME *game;
 	int reference_count;
 	pthread_mutex_t lock;
-	sem_t semaphore;
 };
 
 
 INVITATION *inv_create(CLIENT *source, CLIENT *target, GAME_ROLE source_role, GAME_ROLE target_role){
 	if(source == target) return NULL;
-	INVITATION *invite = calloc(sizeof(INVITATION), 1); // initial a zero heap space
+	INVITATION *invite = calloc(1,sizeof(INVITATION)); // initial a zero heap space
 	invite -> source = source;
 	client_ref(source,"as source of invitation\n");
 	invite -> target = target;
@@ -34,9 +32,8 @@ INVITATION *inv_create(CLIENT *source, CLIENT *target, GAME_ROLE source_role, GA
 	invite -> state = INV_OPEN_STATE;
 	invite -> source_role = source_role;
 	invite -> target_role = target_role;
-	pthread_mutex_init(&(invite -> lock), NULL);
-	sem_init(&(invite->semaphore), 0 ,0);
 	inv_ref(invite,"for newly created invite\n");
+	pthread_mutex_init(&(invite -> lock), NULL);
 	return invite;
 }
 
@@ -54,13 +51,16 @@ void inv_unref(INVITATION *inv, char *why){
 	if(inv == NULL) return;
 	pthread_mutex_lock(&inv->lock);
 	inv->reference_count--; // increase the invitation count
+	debug("Decrease reference count on invitation (%d -> %d) %s",inv->reference_count+1, inv->reference_count,why);
 	if(inv->reference_count == 0){// if the reference count is zero then free the invitation
-		 client_unref(inv->source, "invitation is freed\n");
-		 client_unref(inv->target, "invitation is freed\n");
-		 if(!inv->game) game_unref(inv->game,"invitation is freed\n");
+		 if(inv->game) game_unref(inv->game,"invitation is freed\n");
+		 if(inv->source) client_unref(inv->source,"invitation is freed\n");
+		 if(inv->target) client_unref(inv->target,"invitation is freed\n");
+		 pthread_mutex_unlock(&inv->lock);
+		 pthread_mutex_destroy(&inv->lock);
 		 free(inv);
+		 return;
 	}
-	debug("Decrease reference count on invitation (%d -> %d) %s",inv->reference_count, inv->reference_count+1,why);
 	pthread_mutex_unlock(&inv->lock);
 }
 
@@ -90,11 +90,12 @@ GAME *inv_get_game(INVITATION *inv){
 }
 
 int inv_accept(INVITATION *inv){
-	if(inv == NULL || inv->state == INV_OPEN_STATE) return -1;
+	if(inv == NULL || inv->state != INV_OPEN_STATE) return -1;
 	pthread_mutex_lock(&inv->lock);
 	inv->state = INV_ACCEPTED_STATE; // change to accepted state
 	GAME *new_game = game_create(); // create a new game
 	if(!new_game) {pthread_mutex_unlock(&inv->lock);return -1;}
+	inv->game = new_game;
 	pthread_mutex_unlock(&inv->lock);
 	return 0;
 }
@@ -102,8 +103,11 @@ int inv_accept(INVITATION *inv){
 int inv_close(INVITATION *inv, GAME_ROLE role){
 	if(inv == NULL || inv->state == INV_CLOSED_STATE) return -1;
 	pthread_mutex_lock(&inv->lock);
-	inv->state = INV_CLOSED_STATE;
-	if(!game_resign(inv->game,role)){ pthread_mutex_unlock(&inv->lock); return -1;}
+	if(inv->state == INV_ACCEPTED_STATE){ // only resign if the game is in progress
+		if(game_resign(inv->game,role)){ pthread_mutex_unlock(&inv->lock); return -1;} // if game resign reuslt in error otherwise resign
+	}
+	inv->state = INV_CLOSED_STATE; // change to close state
+	//inv->game = NULL;
 	pthread_mutex_unlock(&inv->lock);
 	return 0;
 }
